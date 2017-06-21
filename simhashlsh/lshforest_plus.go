a49ff38443c7ed8b083536ed2d62577292f8ef6a
iamfn <nrgsn82@yahoo.com>
2017-06-21 15:31:45 -0400

    minor fix

--- simhashlsh/lshforest_plus.go
+++ simhashlsh/lshforest_plus.go
@@ -14,10 +14,11 @@ type UnionPair struct {
 // in un-sorted order, given a list of points.
 // Return candidate keys given the query signatures.
 func (index *CosineLSH) QueryPlus(point [][]float64, done <-chan struct{}) <-chan UnionPair {
-	return index.queryPlus(point, index.cosineLSHParam.k, done)
+	return index.queryPlusPlus(point, done)
+	//return index.queryPlus(point, done)
 }
 
-func (index *CosineLSH) queryPlus(points [][]float64, minK int, done <-chan struct{}) <-chan UnionPair {
+func (index *CosineLSH) queryPlus(points [][]float64, done <-chan struct{}) <-chan UnionPair {
 	out := make(chan UnionPair)
 	go func() {
 		defer close(out)
@@ -70,6 +71,75 @@ func (index *CosineLSH) queryPlus(points [][]float64, minK int, done <-chan stru
 				seens[qk] = true
 			}
 		}
+	}()
+	return out
+}
+
+func (index *CosineLSH) queryPlusPlus(points [][]float64, done <-chan struct{}) <-chan UnionPair {
+	out := make(chan UnionPair)
+	// Generate hash keys
+	Hs := make([][]string, len(points))
+	for i := 0; i < len(points); i++ {
+		Hs[i] = index.toBasicHashTableKeys(index.hash(points[i]))
+	}
+	seens := make(map[UnionPair]bool)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for K := index.cosineLSHParam.k; K >= 0; K-- {
+			for rp := range index.probe(Hs, K, done) {
+				if _, seen := seens[rp]; seen {
+					continue
+				}
+				seens[rp] = true
+				out <- rp
+			}
+		}
+	}()
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func (index *CosineLSH) probe(Hs [][]string, prefixSize int, done <-chan struct{}) <-chan UnionPair {
+	out := make(chan UnionPair)
+	var wg sync.WaitGroup
+	wg.Add(index.cosineLSHParam.l * len(Hs))
+	go func() {
+		for i := 0; i < index.cosineLSHParam.l; i++ {
+			for p := 0; p < len(Hs); p++ {
+				go func(i, p int) {
+					hk := Hs[p][i]
+					ht := index.tables[i]
+					defer wg.Done()
+					k := sort.Search(len(ht), func(x int) bool {
+						return ht[x].hashKey[:prefixSize] >= hk[:prefixSize]
+					})
+					if k < len(ht) && ht[k].hashKey[:prefixSize] == hk[:prefixSize] {
+						for j := k; j < len(ht) && ht[j].hashKey[:prefixSize] == hk[:prefixSize]; j++ {
+							for _, key := range ht[j].keys {
+								rp := UnionPair{
+									QueryIndex:   p,
+									CandidateKey: key,
+								}
+								select {
+								case out <- rp:
+								case <-done:
+									return
+								}
+							}
+						}
+					}
+				}(i, p)
+			}
+		}
+	}()
+	go func() {
+		wg.Wait()
+		close(out)
 	}()
 	return out
 }
