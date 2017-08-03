48fb81121487197ea28bd59833cce4add8a18bc5
iamfn <nrgsn82@yahoo.com>
2017-08-02 23:31:05 -0400

    precision recall plots

--- plotting/opendata/avg_precision_recall_various_k.py
+++ plotting/opendata/avg_precision_recall_various_k.py
@@ -5,7 +5,20 @@ import matplotlib.pyplot as plt
 import sqlite3
 import os
 
-def get_measure_accuracy(dbname, tablename):
+# first we count the number of queries that are answered by some
+# measure.
+def get_answered_queries(dbnames, tablenames):
+    queries = {}
+    for i in range(len(dbnames)):
+        db = dbnames[i]
+        table = tablenames[i]
+        db = sqlite3.connect(db)
+        cursor = db.cursor()
+        for query_name in cursor.execute("select distinct query_table from " + table + ";").fetchall():
+            queries[query_name] = 1
+    return queries
+
+def get_measure_accuracy(dbname, tablename, queries):
     print(tablename)
     db = sqlite3.connect(dbname)
     cursor = db.cursor()
@@ -13,12 +26,10 @@ def get_measure_accuracy(dbname, tablename):
     rs = []
     queryNum = 0
     for n in ns:
-    #for query_table in cursor.execute("select distinct query_table from " + tablename + ";").fetchall():
         queryNum += 1
         correct = {}
         returned = {}
         for query_table, count in cursor.execute("select query_table, count(candidate_table) as correct from (select distinct query_table, candidate_table from " + tablename + " where n<=" + str(n) + ") natural join groundtruth group by query_table;").fetchall():
-        #for count in cursor.execute("select count(candidate_table) as correct from  (select distinct query_table, candidate_table from " + tablename + " where query_table='" + query_table[0] + "' order by alignment_probability desc) natural join groundtruth;").fetchall():
             queryNum += 1
             correct[query_table] = count
         for query_table, total in cursor.execute("select query_table, count(*) as total from (select distinct query_table, candidate_table from " + tablename + " where n<=" + str(n) + ") group by query_table;").fetchall():
@@ -28,9 +39,9 @@ def get_measure_accuracy(dbname, tablename):
         for q, s in returned.items():
             if q in correct:
                 sumPrecision += (float(correct[q])/float(s))
-                sumRecall += (float(correct[q])/float(max(ns)))
+                sumRecall += (float(correct[q])/float(len(ns)))
         ps.append(sumPrecision/float(len(returned)))
-        rs.append(sumRecall/float(len(returned)))
+        rs.append(sumRecall/float(len(queries)))
     print("Done precision...")
     return ps, rs
 
@@ -44,12 +55,15 @@ legends = []
 all_ps = []
 all_rs = []
 benchmarkPath = "/home/fnargesian/TABLE_UNION_OUTPUT/benchmark-v4"
-measures = ["Set", "Sem-Set", "Natural Language", "Sem"]
+measures = ["Set", "Sem-Set", "NL", "Sem"]
 dbs = [os.path.join(benchmarkPath, "jaccard-experiments.sqlite"), os.path.join(benchmarkPath, "ont-jaccard-experiments.sqlite"), os.path.join(benchmarkPath, "emb-experiments.sqlite"), os.path.join(benchmarkPath,"ont-jaccard-experiments.sqlite")]
-tables = ["jaccard_fixedn", "ontology_fixedn_backup", "t2_fixedn_backup", "pure_ontology_fixedn_backup"]
+tables = ["jaccard_fixedn", "ontology_fixedn", "t2_fixedn_backup", "pure_ontology_fixedn"]
 markers = ["x", "+", "*", "o"]
+
+answeredQueries = get_answered_queries(dbs, tables)
+
 for i in range(len(dbs)):
-    ps, rs = get_measure_accuracy(dbs[i],tables[i])
+    ps, rs = get_measure_accuracy(dbs[i],tables[i], answeredQueries)
     all_ps.append(ps)
     all_rs.append(rs)
 
@@ -65,15 +79,13 @@ ax.grid(linestyle='--')
 ax.set_xlabel('Recall', fontsize=fs)
 ax.set_ylabel('Precision', fontsize=fs)
 ax.set_xlim([0,1])
-ax.set_ylim([0,1])
+ax.set_ylim([0.5,1])
 ax.set_axisbelow(True)
 
 for i in range(len(dbs)):
     ax.plot(all_rs[i], all_ps[i], linestyles[i], label=measures[i],  markersize=markersize, color = cs[i], linewidth=lw)
 lgd = plt.legend(ncol=1, loc="lower right", bbox_transform=plt.gcf().transFigure, fontsize=fs)
-#lgd = plt.legend(ncol=1, mode="expand", loc="upper left", bbox_to_anchor=(0.90, 0.87, 0.28, -0.05),           bbox_transform=plt.gcf().transFigure, fontsize=fs)
 plt.savefig(args.outputa, bbox_extra_artists=(lgd,), bbox_inches='tight', pad_inches=0.02)
 plt.close()
 print("Done.")
-
 
--- plotting/opendata/overall_precision_recall.py
+++ plotting/opendata/overall_precision_recall.py
@@ -4,7 +4,24 @@ import sqlite3
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
-def get_precision_recall(dbname, tablename):
+
+# since some queries do not have any answer,
+# first we count the number of queries that are answered by some
+# measure.
+def get_answered_queries(dbnames, tablenames):
+    queries = {}
+    for i in range(len(dbnames)):
+        db = dbnames[i]
+        table = tablenames[i]
+        db = sqlite3.connect(db)
+        cursor = db.cursor()
+        for query_name in cursor.execute("select distinct query_table from " + table + ";").fetchall():
+            queries[query_name] = 1
+    return queries
+
+
+def get_precision_recall(dbname, tablename, answeredQueries):
+    print(tablename)
     db = sqlite3.connect(dbname)
     cursor = db.cursor()
     returned = 0
@@ -18,7 +35,7 @@ def get_precision_recall(dbname, tablename):
         correct = c[0]
     print("allcorrect: %d returned: %d correct: %d" %(allcorrect, returned, correct))
     return float(correct)/float(returned), float(correct)/float(allcorrect)
-
+    #return float(correct)/float(returned), float(correct)/float(len(answeredQueries)*25)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-output", "--output", default="plots/precision_recall.pdf")
@@ -26,14 +43,16 @@ parser.add_argument("-output", "--output", default="plots/precision_recall.pdf")
 args = parser.parse_args(sys.argv[1:])
 #
 benchmarkPath = "/home/fnargesian/TABLE_UNION_OUTPUT/benchmark-v4"
-measures = ["Set", "Sem", "Sem-Set", "NL", "DasSarma"]
+measures = ["Set", "Sem-Set", "Sem", "NL", "DasSarma"]
 dbs = [os.path.join(benchmarkPath, "jaccard-experiments.sqlite"), os.path.join(benchmarkPath, "ont-jaccard-experiments.sqlite"), os.path.join(benchmarkPath, "ont-jaccard-experiments.sqlite"), os.path.join(benchmarkPath, "emb-experiments.sqlite"), os.path.join(benchmarkPath, "sarma.sqlite")]
-tables = ["jaccard_fixedn", "pure_ontology_fixedn_backup", "ontology_fixedn_backup", "t2_fixedn_backup", "topk_sarma"]
+tables = ["jaccard_fixedn_backup", "ontology_fixedn_backup", "pure_ontology_fixedn_backup", "t2_fixedn_backup", "topk_sarma"]
+#
+answeredQueries = get_answered_queries(dbs, tables)
 #
 ps = []
 rs = []
 for i in range(len(dbs)):
-    p, r = get_precision_recall(dbs[i],tables[i])
+    p, r = get_precision_recall(dbs[i],tables[i], answeredQueries)
     ps.append(p)
     rs.append(r)
 #
@@ -49,17 +68,17 @@ ax.set_xlim([0,1])
 ax.set_ylim([0,1])
 ax.set_axisbelow(True)
 for i in range(len(dbs)):
-    plt.scatter(rs[i], ps[i], color=cs[i], edgecolor = 'black', marker='o', alpha=0.5, rasterized=True, s=10**2, label = measures[i])
+    plt.scatter(rs[i], ps[i], color=cs[i], edgecolor = 'black', marker='o', alpha=0.5, s=10**2, label = measures[i])
     if i == 0:
-        ax.annotate(measures[i], (rs[i]-dist,ps[i]), ha="right", va="bottom", fontsize=fs)
+        ax.annotate(measures[i], (rs[i]-dist,ps[i]-dist), ha="right", va="bottom", fontsize=fs)
     if i == 2:
-        ax.annotate(measures[i], (rs[i],ps[i]+dist), ha="right", va="bottom", fontsize=fs)
+        ax.annotate(measures[i], (rs[i]-dist,ps[i]-dist), ha="right", va="bottom", fontsize=fs)
     if i == 1:
-        ax.annotate(measures[i], (rs[i],ps[i]-2*dist), ha="right", va="bottom", fontsize=fs)
+        ax.annotate(measures[i], (rs[i]+3.75*dist,ps[i]-2.25*dist), ha="right", va="bottom", fontsize=fs)
     if i == 3:
-        ax.annotate(measures[i], (rs[i]+3*dist,ps[i]), ha="right", va="bottom", fontsize=fs)
+        ax.annotate(measures[i], (rs[i]+3*dist,ps[i]-dist), ha="right", va="bottom", fontsize=fs)
     if i == 4:
-        ax.annotate(measures[i], (rs[i]+dist, ps[i]), ha="right", va="bottom", fontsize=fs)
+        ax.annotate(measures[i], (rs[i]+7*dist, ps[i]+dist), ha="right", va="bottom", fontsize=fs)
 plt.savefig(args.output, bbox_inches='tight', pad_inches=0.02)
 plt.close()
 #
